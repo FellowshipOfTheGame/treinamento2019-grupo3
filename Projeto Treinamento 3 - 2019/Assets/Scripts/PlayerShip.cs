5a16127306bb440cd01a56bc8ef89f0ca8cb4abe
Mateus Prado <mateus.prado@usp.br>
2019-08-27 21:50:18 -0300

    Movimento básico da nave e tiro simples

--- Projeto Treinamento 3 - 2019/Assets/Scripts/PlayerShip.cs
+++ Projeto Treinamento 3 - 2019/Assets/Scripts/PlayerShip.cs
@@ -4,49 +4,83 @@ using UnityEngine;
 
 public class PlayerShip : MonoBehaviour{
 
-    public float speed = 5f;
-    public float min_Y, max_Y;
+    public float vertical_speed, horizontal_front_speed, horizontal_back_speed, diagonal_speed;
+    private float speed;
+    public float min_y, max_y, min_x, max_x;
+
+    [SerializeField]
+    private GameObject simple_bullet;
+
+    [SerializeField]
+    private Transform bullet_spawn;
+
+    public float simple_bullet_timer;
+    private float attack_timer;
+    private bool can_attack_simple;
 
     // Start is called before the first frame update
     void Start(){
-        
+
+        can_attack_simple = true;
+
     }
 
     // Update is called once per frame
     void Update(){
 
         MovePlayer();
+        Shoot();
 
     }
 
     void MovePlayer(){
 
+
         //Basic Input(need to change to xbox controllers later)
-        if(Input.GetAxisRaw("Vertical") > 0f){
+        Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0f);
 
-            Vector3 temp = transform.position;
-            temp.y += speed * Time.deltaTime;
-
-            if (temp.y <= max_Y){
-                transform.position = temp;
-            }
-                
-
+        //Define the speed of the movement
+        if(direction.x == 0f && direction.y != 0f) {
+            speed = vertical_speed;
+        }else if(direction.x != 0f && direction.y == 0) {
+            if (direction.x > 0) speed = horizontal_front_speed;
+            else speed = horizontal_back_speed;
+        } else {
+            speed = diagonal_speed;
         }
-        else if(Input.GetAxisRaw("Vertical") < 0f){
 
-            Vector3 temp = transform.position;
-            temp.y -= speed * Time.deltaTime;
+        //move
+        transform.Translate(speed * direction.normalized * Time.deltaTime);
 
-            if (temp.y >= min_Y){
-                transform.position = temp;
-            }
-
-        }
+        //make sure that the spaceship doesnt get out of the screen
+        Vector3 aux_position = transform.position; 
+        aux_position.x = Mathf.Clamp(aux_position.x, min_x, max_x);
+        aux_position.y = Mathf.Clamp(aux_position.y, min_y, max_y);
+        transform.position = aux_position;
 
     }
 
+    void Shoot() {
 
-} //class
+        attack_timer += Time.deltaTime;
+
+        if(attack_timer > simple_bullet_timer) {
+            can_attack_simple = true;
+        }
+
+        //Basic Input(need to change to xbox controllers later)
+        if (Input.GetKey(KeyCode.Space)) {
+            //if the space button was pressed
+            if (can_attack_simple) {
+                can_attack_simple = false;
+                attack_timer = 0f;
+                //Instantiate the simple bullet object
+                Instantiate(simple_bullet, bullet_spawn.position, Quaternion.identity);        
+            }
+        }        
+
+    }
+
+}
 
 
---
+++ Projeto Treinamento 3 - 2019/Assets/Scripts/SimpleBullet.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleBullet : MonoBehaviour{
+
+    public float speed;
+    public float deactivate_timer;
+
+    // Start is called before the first frame update
+    void Start(){
+        //prepare the object to be deactivated
+        Invoke("DeactivateGameObject", deactivate_timer);
+    }
+
+    // Update is called once per frame
+    void Update(){
+        Move();
+    }
+
+
+    void Move() {
+
+        //Just go to the right direction in the screen
+        transform.Translate(Vector3.right * Time.deltaTime * speed);
+
+    }
+
+    void DeactivateGameObject() {
+        gameObject.SetActive(false);
+    }
+    
+
+}
