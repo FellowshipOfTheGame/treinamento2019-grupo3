d0d6f92a942bc225c17193ac9bb7e68b5aa69672
George Maneta <georgemaneta@gmail.com>
2019-09-01 21:30:03 -0300

    Rework do Grid, agora com rezise por resolução

---
+++ Projeto Treinamento 3 - 2019/Assets/Scripts/GridScript.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridScript : MonoBehaviour {
+
+	// Grid
+	public float screenSizePercentX = 0.2f;
+	public float screenSizePercentY = 1f;
+	public float screenOffsetPercentX = 0.9f;
+	public float screenOffsetPercentY = 0.5f;
+	public int numberOfGridRows;
+	public int numberOfGridColumns;
+
+	public float[] gridRowsPositions;
+	public float[] gridColumnsPositions;
+
+	// Cursor e controles
+	public GameObject cursor;
+	public int cursorX = 0;
+	public int cursorY = 0;
+	public float cursorSpeed = 10;
+	public KeyCode upKey;
+	public KeyCode downKey;
+	public KeyCode leftKey;
+	public KeyCode rightKey;
+	public KeyCode action1Key;
+
+	// Outros
+	public Camera mainCamera;
+
+    void Start(){
+        
+    }
+
+    void Update(){
+        AjustToScreenSize();
+        CursorInput();
+       	RefreshCursorPosition();
+    }
+
+    void CursorInput(){
+
+    	if (Input.GetKeyDown(upKey)) cursorY++;
+    	if (Input.GetKeyDown(downKey)) cursorY--;
+    	if (Input.GetKeyDown(leftKey)) cursorX--;
+    	if (Input.GetKeyDown(rightKey)) cursorX++;
+    	cursorX = Mathf.Clamp(cursorX, 0, numberOfGridColumns-1);
+    	cursorY = Mathf.Clamp(cursorY, 0, numberOfGridRows-1);
+    }
+    void RefreshCursorPosition(){
+    	Vector3 posDiff = new Vector3(gridColumnsPositions[cursorX],gridRowsPositions[cursorY],0) - cursor.transform.position;
+    	cursor.transform.Translate(cursorSpeed * posDiff * Time.deltaTime);
+    }
+
+    void AjustToScreenSize(){
+
+        gridRowsPositions = new float[numberOfGridRows];
+        gridColumnsPositions = new float[numberOfGridColumns];
+
+    	int camPixelX = mainCamera.pixelWidth;
+    	int camPixelY = mainCamera.pixelHeight;
+    	
+    	// for Rows 
+    	for (int i = 0; i < numberOfGridRows; i++){
+    		float pixelPos = camPixelY*(screenOffsetPercentY - (screenSizePercentY/2));
+    		pixelPos += (camPixelY*screenSizePercentY)*(i+0.5f)/numberOfGridRows;
+    		gridRowsPositions[i] = mainCamera.ScreenToWorldPoint(new Vector3(0,pixelPos,0)).y;
+    	}
+
+    	// for Columns
+    	for (int i = 0; i < numberOfGridColumns; i++){
+    		float pixelPos = camPixelX*(screenOffsetPercentX - (screenSizePercentX/2));
+    		pixelPos += (camPixelX*screenSizePercentX)*(i+0.5f)/numberOfGridColumns;
+    		gridColumnsPositions[i] = mainCamera.ScreenToWorldPoint(new Vector3(pixelPos,0,0)).x;
+    	}
+    }
+
+    void OnDrawGizmos(){
+    	Gizmos.color = Color.red;
+    	for (int x = 0; x < gridRowsPositions.Length; x++){
+    		for (int y = 0; y < gridColumnsPositions.Length; y++){
+    			Gizmos.DrawSphere(new Vector3(gridColumnsPositions[y],gridRowsPositions[x],0),0.5f);
+    		}
+    	}
+    }
+}
