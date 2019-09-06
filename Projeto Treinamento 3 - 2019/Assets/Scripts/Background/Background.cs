746f34edbc9e04f253d9ef9336a7d35999216d40
PedroFDarela <pedrofdarela@gmail.com>
2019-09-06 03:15:36 -0300

    Efeito de fundo

---
+++ Projeto Treinamento 3 - 2019/Assets/Scripts/Background/Background.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    /// <summary>background offset speed</summary>
+	[SerializeField] private float speed = 3;
+
+    /// <summary>renderer obj 3D</summary>
+    [SerializeField] private Renderer Img;
+
+    void Update()
+    {
+        Offset();
+    }
+
+    private void Offset()
+    {
+        Vector2 offset = new Vector2(speed * Time.deltaTime, 0);
+        Img.material.mainTextureOffset += offset;//translates the texture of the material according to the offset vector
+    }
+}
