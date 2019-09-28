296d8e8f9a9d2427cac7767d1a489ed408b51da7
GeorgeManeta <georgemaneta@gmail.com>
2019-09-28 17:25:36 -0300

    Arrumação de detalhes, nome dos objetos, caixa de colisão externa

--- Projeto Treinamento 3 - 2019/Assets/Scripts/CollisionWall/CollisionWall.cs
+++ Projeto Treinamento 3 - 2019/Assets/Scripts/CollisionWall/CollisionWall.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CollisionWall : MonoBehaviour{
-    
     private void OnTriggerEnter2D(Collider2D collision){
-        if(collision.tag != "Beam") Destroy(collision.gameObject);
-    }
-    
+        if(collision.tag != "Beam"){
+            Destroy(collision.gameObject);
+        }
+    }    
 }
