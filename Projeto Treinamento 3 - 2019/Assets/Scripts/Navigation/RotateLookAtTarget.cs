738552a1bebaa7a8ba696d94d6a4d9f2272fa034
GeorgeManeta <georgemaneta@gmail.com>
2019-10-27 18:26:55 -0300

    fix no LootToTarger e translateForward

--- Projeto Treinamento 3 - 2019/Assets/Scripts/Navigation/RotateLookAtTarget.cs
+++ Projeto Treinamento 3 - 2019/Assets/Scripts/Navigation/RotateLookAtTarget.cs
@@ -10,9 +10,15 @@ public class RotateLookAtTarget : MonoBehaviour
 
     void FixedUpdate()
     {
+        
+        Vector3 targetDirection = target.transform.position - gameObject.transform.position;
+        float angleDiference = Vector3.SignedAngle(transform.right, targetDirection, Vector3.forward);
+        transform.Rotate(0,0,angleDiference*lookSpeed*Time.fixedDeltaTime,Space.Self);
+        /*
         // rotate to aling transform Y axis with target
         Vector3 targetDirection = target.transform.position - gameObject.transform.position;
         Vector3 rotateAxis = Vector3.Cross(transform.right, targetDirection);
-        transform.RotateAround(transform.position, rotateAxis, lookSpeed * Time.fixedDeltaTime);
+        transform.RotateAround(transform.position, rotateAxis, rotateAxis.z * lookSpeed * Time.fixedDeltaTime); 
+        */
     }
 }
--- Projeto Treinamento 3 - 2019/Assets/Scripts/Navigation/TranslateForwardYAxis.cs
+++ Projeto Treinamento 3 - 2019/Assets/Scripts/Navigation/TranslateForwardYAxis.cs
@@ -8,6 +8,6 @@ public class TranslateForwardYAxis : MonoBehaviour
     private float speed = 5;
     void FixedUpdate()
     {
-        transform.Translate(speed * transform.right * Time.fixedDeltaTime);
+        transform.Translate(speed * Vector3.right * Time.fixedDeltaTime);
     }
 }
