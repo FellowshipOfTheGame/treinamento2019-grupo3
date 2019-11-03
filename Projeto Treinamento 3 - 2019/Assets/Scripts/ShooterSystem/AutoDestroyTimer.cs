47f4cc1afbaf493353dd93d7853917b29f55f186
Mateus Prado <mateus.prado@usp.br>
2019-11-03 15:47:12 -0300

    ScatterShot

---
+++ Projeto Treinamento 3 - 2019/Assets/Scripts/ShooterSystem/AutoDestroyTimer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestroyTimer : MonoBehaviour{
+    public float timer = 5f;
+    // Start is called before the first frame update
+    void Start(){
+        Invoke("AutoDestroy", timer);        
+    }
+    private void AutoDestroy(){
+        Destroy(gameObject);
+    }
+}
