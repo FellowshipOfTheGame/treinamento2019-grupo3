fa22f5c6cc8a757657fc71afb25e26e9224dca86
Mateus Prado <mateus.prado@usp.br>
2019-11-04 15:19:43 -0300

    Colocando alguns sprites

---
+++ Projeto Treinamento 3 - 2019/Assets/Scripts/ChooseRandomSprite/ChooseRandomSprite.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseRandomSprite : MonoBehaviour{
+    public GameObject[] spritesObjects;
+    void Start(){
+        Instantiate(spritesObjects[Random.Range(0, spritesObjects.Length-1)], transform);
+        //transform.GetChild().gameObject.SetActive(true);
+    }
+}
