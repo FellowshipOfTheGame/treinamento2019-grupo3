3cba6b8375e86ffcdb908a335f6e0e9860f27fbc
Mateus Prado <mateus.prado@usp.br>
2019-11-05 21:37:10 -0300

    Animator PlayerShip1 apenas

--- Projeto Treinamento 3 - 2019/Assets/Scripts/Players/PlayerShipMovement.cs
+++ Projeto Treinamento 3 - 2019/Assets/Scripts/Players/PlayerShipMovement.cs
@@ -7,14 +7,16 @@ public class PlayerShipMovement : MonoBehaviour
     public float speed = 1f;
     public float speedMultiplier = 1f;
     public Vector3 direction = new Vector3(0,0,0);
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start(){
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void FixedUpdate(){
+        animator.SetFloat("VelocityY", direction.y);
         transform.Translate(speed * speedMultiplier * direction.normalized * Time.fixedDeltaTime);
     }
 
